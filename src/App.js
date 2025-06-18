f61fab9cc1a653b270f29b17f9d750665f154273
ashkhen000 <ashkhen978@gmail.com>
2025-06-18 12:34:01 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       const token = await recaptchaRef.current.executeAsync();
       recaptchaRef.current.reset();
 
-      const res = await fetch('http://pythonapi-4-evt9.onrender.com/register', {
+      const res = await fetch('https://pythonapi-4-evt9.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
